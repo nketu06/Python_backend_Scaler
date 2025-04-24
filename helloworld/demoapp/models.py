f6b874861ce531673af52af0faa333a20072754b
Nishant ketu <nketu06@gmail.com>
2025-04-25 00:23:37 +0530

    added student model

--- helloworld/demoapp/models.py
+++ helloworld/demoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    dob = models.DateField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
