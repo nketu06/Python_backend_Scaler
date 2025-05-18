24e59b4fd0968d8511024db86753feba21e31fc1
Nishant ketu <nketu06@gmail.com>
2025-05-18 21:27:16 +0530

    add doc admin

--- Exception and Decorator/main.py
+++ Exception and Decorator/main.py
@@ -13,25 +13,49 @@ def testing():
     finally:
         print("E")
 
+
 def decorator(func):
     def wrapper(*args,**kwargs):
-        print("hello", end=" ")
+        print("this is args of deco",args,kwargs)
+        print("hello")
         func(*args,**kwargs)
         print("bye")
     return wrapper
 
 
+def is_admin(func):
+    def wrapper(*args,**kwargs):
+        print("this is args of isadmin",args,kwargs)
+        if not args[0]:
+            print(f"{args[1]} is not a admin!")
+            return False
+        return func(args[1])
+    return wrapper
+
+
+
 @decorator
 def printname1(name):
-    print(name,end=" ")
+    print(name)
+
+@is_admin
+def printname2(name):
+    print(name)
+
 
 @decorator
-def printname2(name):
-    print(name,end=" ")
-
+@is_admin
+def printname3(name):
+    print(name)
+# printname3 = decorator(is_admin(printname3))
+# printname3(False,"k")
 
 
 if __name__ == '__main__':
     # testing()
-    printname1("ketu")
-    printname1("sanjana")
+    # printname1("ketu")
+    # printname2(True,"sanjana")
+    printname3(False,"time")
+
+
+
--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'demoapp',
-    'rest_framework'
+    # 'rest_framework'
 ]
 
 MIDDLEWARE = [
